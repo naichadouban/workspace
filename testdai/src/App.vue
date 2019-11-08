0c0f2ec3a44bfa8b787954c79e28de7968e4c8c6
naichadouban <naichadouban@gmail.com>
2019-11-08 10:32:00 +0800

    addtestdai

---
+++ testdai/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+      <helloMeta />
+  </div>
+</template>
+
+<script>
+    import helloMeta from "./components/hello-meta"
+export default {
+  name: "app",
+  components: {
+    helloMeta
+  },
+  beforeCreate () {
+    console.log('registerWeb3 Action dispatched from casino-dapp.vue')
+    this.$store.dispatch('registerWeb3')
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ testdai/src/components/hello-meta.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>hello Metamask</h1>
+    <p>Metamask: {{ web3.isInjected }}</p>
+    <p>Network: {{ web3.networkId }}</p>
+    <p>Account: {{ web3.coinbase }}</p>
+    <p>Balance: {{ web3.balance }}</p>
+</template>
+
+<script>
+  export default {
+    name: "hello-meta",
+    computed:{
+      web3(){
+        return this.$store.state.web3
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
